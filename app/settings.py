1f935a4f6d00441f860ea48ba667c9fe9026903a
Lucas Sobral de Morais <lucas.morais@eldoradobrasil.com.br>
2024-09-19 11:15:39 -0400

    allowed_host_all

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gs$@n(^uk7ux6&y3)&l))nh5o*cxbll*cc()k@n$s-!uf8p6kl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
